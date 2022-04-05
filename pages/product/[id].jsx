489029f8719ff916bc9cc0ce4718742c24e7bc2c
pavithiraa <hrpavithraa2000@gmail.com>
2022-04-05 17:54:07 +0530

    updated

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -91,7 +91,7 @@ const Product= ({ pizza }) => {
 
 
 export const getServerSideProps = async ({params}) =>{
-  const  res= await axios.get(`http://localhost:3000/api/products/${params.id}`);
+  const  res= await axios.get(`https://res-nextjs.herokuapp.com/api/products/${params.id}`);
   return{
     props: {
       pizza: res.data,
